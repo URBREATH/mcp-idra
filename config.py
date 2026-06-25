7245fb6d64f40439eadfdf39c75831b051e0c923
maria.magro <maria.magro@demetrix.it>
2026-06-25 12:02:55 +0200

    modified config file

--- config.py
+++ config.py
@@ -2,10 +2,12 @@
 """Configurazione centralizzata e client Ollama condiviso."""
 
 import os
+from dotenv import load_dotenv
 from ollama import Client
 
-OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://localhost:11434")
-PILOT_CITIES = "Aarhus,Athens,Cluj-Napoca,Kajaani,Leuven,Madrid,Parma,Pilsen,Tallinn,Attica,Napoli,Vilnius,Molina"
+load_dotenv()
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://ollama-backend:11434")
+PILOT_CITIES = "Aarhus,Athens,Cluj-Napoca,Kajaani,Leuven,Madrid,Parma,Pilsen,Tallinn"
 IDRA_TAG = "IDRA"
 
 MODELS = {
@@ -13,7 +15,5 @@ MODELS = {
     "2": ("EngGPT", "enggpt", os.getenv("ENGGPT_REPO", "engineering-group/EngGPT2-16B-A3B")),
     "3": ("TildeOpen-30b", "TildeOpen-30b", os.getenv("TILDE_REPO", "TildeAI/TildeOpen-30b"))
 }
-
-# ENGGPT_DIR = os.getenv("ENGGPT_DIR", "./enggpt")
 
 ollama_client = Client(host=OLLAMA_HOST)
